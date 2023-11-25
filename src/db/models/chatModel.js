6d5bcc726beec73dd404169e022a09a605a06b91
Wonbin Park <pob1109@gmail.com>
2023-11-25 20:34:44 +0900

    Update : chat

--- src/db/models/chatModel.js
+++ src/db/models/chatModel.js
@@ -2,32 +2,58 @@ import { ChatSchema } from "../schemas/chatSchema.js";
 import mongoose from "mongoose";
 
 export const Chat = mongoose.model("Chat",ChatSchema)
+const ObjectId = mongoose.Types.ObjectId;
 
 class ChatModel {
-    async sendMessage(data){
+    async makeChatRoom(nick1,nick2){
         try{
             const chatData = {
-                nickname : data.nickname,
-                with : data.with,
-                chat : data.chat,   
+                nickname : nick1,
+                with : nick2,
+                chat : [],   
 /*chat형태 
 [{sendedBy:"elice", timeStamp:Date.now(),content:'안녕'},
 {sendedBy:"john", timeStamp:Date.now(),content:'안녕'},
 {sendedBy:"elice", timeStamp:Date.now(),content:'가자'}]
 */
             }
-
-            const sendedMessage = await Chat.create()
-            return sendedMessage;
-        }catch(e){
+            await Chat.create(chatData)
+        return;
+        }catch(err){
             throw err;
         }
     }
-    async readMessage(){
+
+    async getChatRoom(){
         try{
-            const sendedMessage = await Chat.find({})
+            const chatRoomData = await Chat.find({})
+            return chatRoomData;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async sendMessage(chatRoomId,data){
+        try{
+            
+            await Chat.findByIdAndUpdate(new ObjectId(chatRoomId),{$push:{chat:data}})
+            return;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async readMessage(chatRoomId,lastTimestamp){
+        try{
+            const filter ={
+                _id: new ObjectId(chatRoomId),
+                chat: {
+                    $elemMatch: { "timestamp": { $gt: lastTimestamp } }
+                  }
+            }
+            const sendedMessage = await Chat.find(filter)
             return sendedMessage;
-        }catch(e){
+        }catch(err){
             throw err;
         }
     }
@@ -36,5 +62,6 @@ class ChatModel {
 /*  get- 채팅창 모든 내용 불러오기
     post - 채팅 작성
 */ 
+const chatModel= new ChatModel()
 
-export { ChatModel }
+export { chatModel }
